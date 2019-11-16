8e880613ea2c0e360bbbeabe9f935cfa02962d48
aman-malhotra-1999 <official.aman18@gmail.com>
2019-11-17 01:23:38 +0530

    Parents data add and fetch api finalised (SQL)

--- fetch_details/database.py
+++ fetch_details/database.py
@@ -4,10 +4,10 @@ class db:
     #Initializing
     def __init__(self):
         self.connection=False
-        self.host='35.200.232.172'
-        self.user='student360'
-        self.password='student360'
-        self.database='student360'
+        self.host='34.93.63.139'
+        self.user='teeka'
+        self.password='teeka'
+        self.database='teeka'
         self.connect_db()
 
     #Creating Database connection
@@ -18,7 +18,7 @@ class db:
                                     user=self.user,
                                     password=self.password,
                                     db=self.database)
-                #print("Databese connected successfully")
+                print("Databese connected successfully")
             except Exception:
                 return print("Failed to connect database")
     
--- fetch_details/fetch_details.py
+++ fetch_details/fetch_details.py
@@ -8,41 +8,6 @@ import database as db
 
 app=Flask("__main__")
 cors=CORS(app)
-global data, columns
-
-
-# Request 
-#     "sid"   -> student_id for which the data is to be fetched
-#     "table_number"  -> table_number for table from which the data is to be fetched
-#     table_number 1 = "aadhar"
-#     table_number 2 = "collegeid"
-#     table_number 3 = "gradecard"
-#     table_number 4 = "pan"
-#     table_number 5 = "photo"
-#     table_number 6 = "provider_user_info"
-#     table_number 7 = "registration_check"
-#     table_number 8 = "resume"
-#     table_number 9 = "student_academic_details"
-#     table_number 10 = "student_info"
-#     table_number 11 = "tenthmarksheet"
-#     table_number 12 = "twelfthmarksheet"
-
-table = {}
-table["1"] = "aadhar"
-table["2"] = "collegeid"
-table["3"] = "gradecard"
-table["4"] = "pan"
-table["5"] = "photo"
-table["6"] = "provider_user_info"
-table["7"] = "registration_check"
-table["8"] = "resume"
-table["9"] = "student_academic_details"
-table["10"] = "student_info"
-table["11"] = "tenthmarksheet"
-table["12"] = "twelfthmarksheet"
-table["13"] = "github_data"
-table["14"] = "linkedin_data"
-
 
 # ------------- FETCH THE DETAILS OF EXISTING USER ------------ #
 @app.route('/fetch_details',methods=['POST'])
@@ -52,72 +17,43 @@ def fetch_user_details():
     user_data = None
     try:
         data=request.get_json()
-        id = data['sid']
-        table_number = data['table_number']
-        user_data = get_details(id, table_number)
-        columns = get_column_names(id, table_number)
-        dict = {}
-        for c, u in zip(columns,user_data):
-            dict[c]=u
-        return jsonify(dict)
+        name = data['name']
+        gender = data['gender']
+        dob = data['dob']
+        age = data['age']
+        nationality = data['nationality']
+        city = data['city']
+        state = data['state']
+        profession = data['profession']
+        income = data['income']
+        response = saveDetails(data, name, gender, age, nationality, city, state, income, profession, dob)
+        return jsonify({"resonse":response})
     except Exception as err:
         return jsonify({"error": str(err)})
 
-#---------------------------------------RUN SQL QUERY FOR FETCHING DETAILS FROM ANY TABLE---------------------------------#
-def get_details(id, table_number):
-    conn=db.db()
-    sql="SELECT * FROM "+table[table_number]+" WHERE student_id = '"+str(id)+"';"
-    cur = conn.exec(sql)
-    data = cur.fetchall()
-    l = []
-    for d in data:
-        for i in d:
-            l.append(i)
-    return l
-
-def get_column_names(id, table_number):
-    conn=db.db()
-    sql = "describe "+table[table_number]+";"
-    cur = conn.exec(sql)
-    columns = cur.fetchall()
-    l = []
-    for c in columns:
-        l.append(c[0])
-    # print(l)
-    return l
-
-# ------------- FETCH THE DETAILS OF EXISTING USER ------------ #
-@app.route('/fetch_all',methods=['POST'])
+@app.route('/getParentData',methods=['POST'])
 @cross_origin(allow_headers=['Content-Type','Authorization'])
-def fetch_all():
-    global table_data
-    table_data = None
+def getP():
     try:
-        data=request.get_json()
-        table_number = data['table_number']
-        table_data = get_all_data(table_number)
-        columns = get_column_names(id, table_number)
-        alldata = []
-        for t in table_data:
-            abc = None
-            dict = {}
-            for c, u in zip(columns,t):
-                
-                abc = c
-                dict[c]=u
-            alldata.append(dict)
-        return jsonify(alldata)
+        response = get_details()
+        return jsonify({"resonse":response})
     except Exception as err:
         return jsonify({"error": str(err)})
-    
+
 #---------------------------------------RUN SQL QUERY FOR FETCHING DETAILS FROM ANY TABLE---------------------------------#
-def get_all_data(table_number):
+def get_details():
     conn=db.db()
-    sql="SELECT * FROM "+table[table_number]+";"
+    sql="SELECT * FROM parents;"
     cur = conn.exec(sql)
     data = cur.fetchall()
-    
     return data
+
+#---------------------------------------RUN SQL QUERY FOR FETCHING DETAILS FROM ANY TABLE---------------------------------#
+def saveDetails(data, name, gender, age, nationality, city, state, income, profession, dob):
+    conn=db.db()
+    sql="INSERT INTO parents(name, age, gender, nationality, city, state, dob, income, profession) values('"+name+"','"+age+"','"+gender+"','"+nationality+"','"+city+"','"+state+"','"+dob+"','"+income+"','"+profession+"');"
+    cur = conn.exec(sql)
+    return "success"
     
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
