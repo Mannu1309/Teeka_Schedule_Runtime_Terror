37a0ee6f5a66b47731529995f15c9185bf5297d2
Mannu1309 <officialuse465@gmail.com>
2019-11-17 05:32:21 +0530

    Added add child porta;

--- teeka/lib/Parents/dashboard.dart
+++ teeka/lib/Parents/dashboard.dart
@@ -3,6 +3,8 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:teeka/Widgets/styles.dart';
 import 'package:teeka/Widgets/title.dart';
 
+import '../Widgets/widgets.dart';
+
 class Dashboard extends StatefulWidget {
   @override
   _DashboardState createState() => _DashboardState();
@@ -89,7 +91,7 @@ class _DashboardState extends State<Dashboard>
           Text(
             "Children Age : 10",
             style: Styles(context).title().copyWith(
-                  fontSize: 20.0,
+                  fontSize: 16.0,
                   fontWeight: FontWeight.w500,
                 ),
           ),
@@ -100,7 +102,7 @@ class _DashboardState extends State<Dashboard>
                 Text(
                   "Pending Vacinations:",
                   style: Styles(context).title().copyWith(
-                        fontSize: 20.0,
+                        fontSize: 14.0,
                         fontWeight: FontWeight.w500,
                       ),
                 ),
@@ -123,7 +125,7 @@ class _DashboardState extends State<Dashboard>
   }
 
   Widget profile() {
-    return Container();
+    return Profile();
   }
 
   @override
@@ -144,13 +146,221 @@ class _DashboardState extends State<Dashboard>
         automaticallyImplyLeading: false,
         title: Text(
           "Parents Dashboard",
-          style: Styles(context).title(),
+          style: Styles(context).title().copyWith(
+            fontSize: 18.0
+          ),
+
         ),
         elevation: 0.0,
         backgroundColor: Colors.transparent,
       ),
       body: parentsBody(),
       bottomNavigationBar: tabBar,
+    );
+  }
+}
+
+class Profile extends StatefulWidget {
+  @override
+  _ProfileState createState() => _ProfileState();
+}
+
+class _ProfileState extends State<Profile> {
+  Widget detailsTile(String prop, String value){
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text(prop, style: Styles(context).title(),),
+        ), 
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text(value,style: Styles(context).title(),),
+        )
+      ],
+    );
+  }
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+     children: <Widget>[
+       ListTile(
+         title: Text("Add child details"),
+         trailing: Icon(Icons.add),
+         onTap: (){
+           Navigator.push(context, MaterialPageRoute(
+            builder: (context)=>StudentForm() 
+           ));
+         },
+       ),
+       detailsTile("Name :", "Aman"),
+       detailsTile("Age :", "20"),
+       detailsTile("Gender :", "Male"),
+       detailsTile("Date Of Birth :", "18-01-1999"),
+       detailsTile("Profession :", "Student"),
+       detailsTile("Income :", "Income"),
+       detailsTile("Address :", "City, State"),
+
+     ], 
+    );
+  }
+}
+
+class StudentForm extends StatefulWidget {
+  @override
+  _StudentFormState createState() => _StudentFormState();
+}
+
+class _StudentFormState extends State<StudentForm> {
+  String selectedG;
+  DateTime date;
+  
+
+Widget gender() {
+    return new DropdownButton<String>(
+      hint: Text(
+        "Gender *",
+        style: Styles(context).subTitle().copyWith(
+              color: Colors.grey.shade600,
+            ),
+      ),
+      value: selectedG,
+      style: Styles(context).subTitle().copyWith(
+            color: Colors.grey.shade700,
+          ),
+      items: <String>[
+        'Male',
+        'Female',
+      ].map((String value) {
+        return new DropdownMenuItem<String>(
+          value: value,
+          child: Padding(
+            padding: const EdgeInsets.all(
+              8.0,
+            ),
+            child: new Text(
+              value,
+            ),
+          ),
+        );
+      }).toList(),
+      onChanged: (String str) {
+        selectedG = str;
+        setState(() {});
+      },
+    );
+  }
+  onDateClick() {
+    showDatePicker(
+      context: context,
+      initialDate: date != null ? date : DateTime(DateTime.now().year),
+      firstDate: DateTime(DateTime.now().year - 5),
+      lastDate: DateTime(DateTime.now().year),
+    ).then((DateTime date) {
+      print(date);
+      date = date;
+      setState(() {});
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+     
+    return Scaffold(
+      backgroundColor: Theme.of(context).cardColor,
+      appBar:AppBar(title: Text(
+        "Add Child Details",
+        style: Styles(context).title().copyWith(
+            fontSize: 18.0
+          ),
+      ),
+      elevation: 0.0,backgroundColor: Colors.transparent,
+      iconTheme: IconThemeData(color: Colors.black),),
+      body: Center(
+        child: Card(
+          elevation: 10.0,
+          margin: EdgeInsets.all(10.0),
+                child: ListView(
+                  shrinkWrap: true,
+            children: <Widget>[
+            padding(
+                      formFields(
+                        context: context,
+                        controller: new  TextEditingController(),
+                        hintText: "Name",
+                        validator: (String ) {return null;}
+                      ),
+                    ),
+                    padding(
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: <Widget>[
+                          padding(
+                            gender(),
+                          ),
+                          padding(
+                            FlatButton(
+                              child: Text(
+                                date == null
+                                    ? "Date of Birth *"
+                                    : (date.day.toString() +
+                                        "/" +
+                                        date.month.toString() +
+                                        "/" +
+                                        date.year.toString()),
+                                textAlign: TextAlign.left,
+                                style: Styles(context).subTitle().copyWith(
+                                      color: Colors.grey.shade600,
+                                    ),
+                              ),
+                              onPressed: () {
+                                onDateClick();
+                              },
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    
+                    padding(
+                      formFields(
+                        context: context,
+                        controller: new  TextEditingController(),
+                        hintText: "Birth Weight",
+                        validator: (String ) {return null;}
+                        
+                      ),
+                    ),
+                    padding(
+                      formFields(
+                        context: context,
+                        controller: new  TextEditingController(),
+                        hintText: "Deliver Type",
+                        validator: (String ) {return null;}
+                        
+                      ),
+                    ),
+                    padding(
+                      formFields(
+                        context: context,
+                        controller: new  TextEditingController(),
+                        hintText: "Vaccines Already Done",
+                        validator: (String ) {return null;}
+                        
+                      ),
+                    ),
+                    padding(
+                      button(
+                        context: context,
+                        function: (){},
+                        text: "Add Child Details",
+                      ),
+                    ),
+          ],
+          ),
+        ),
+      ),
     );
   }
 }
